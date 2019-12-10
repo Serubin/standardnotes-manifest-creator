f1a21959760e892e4a1b908e5f5c618b6aee80c4
Serubin <Serubin@Serubin.net>
2019-12-09 21:15:35 -0800

    Update script to use jq

--- sn-manifest.sh
+++ sn-manifest.sh
@@ -85,6 +85,10 @@ function cloneRepo () {
 function generateManifest () {
 
   human_name=$(echo ${name} | tr '-' ' ' | sed -e 's/\b\(.\)/\u\1/g')
+  if [[ $index == "" ]]; then
+    index="index.html"
+  fi
+
   cat >> ${target}/ext.json << EOL
     {
         "identifier": "org.sn-ext.${name}",
@@ -93,6 +97,7 @@ function generateManifest () {
         "area": "${area}",
         "version": "${version}",
         "url": "/extensions/${name}/",
+        "index": "${index}",
         "github": "${repo}"
     }
 EOL
@@ -115,7 +120,8 @@ function getRepoName () {
 function getJsonAttr () {
   file=$1
   attr=$2
-  echo $(cat ${target}/${file} | grep "${attr}" | xargs | sed -e "s/${attr}: //" | sed -e "s/,//")
+  #echo $(cat ${target}/${file} | grep "${attr}" | xargs | sed -e "s/${attr}: //" | sed -e "s/,//")
+  jq "${attr} | select( . != null )" ${target}/${file} | sed -e 's/"//g'
 }
 
 if [[ ${flag_help} == "1" ]]; then
@@ -133,11 +139,13 @@ fi
 cloneRepo
 
 if [[ ${version} == "" ]]; then
-  version=$(getJsonAttr "package.json" "version")
+  version=$(getJsonAttr "package.json" ".version")
 fi
 if [[ ${id} == "" ]]; then
-  id=$(getJsonAttr "package.json" "name")
+  id=$(getJsonAttr "package.json" ".name")
 fi
+
+index=$(getJsonAttr "package.json" ".sn.main")
 
 generateManifest
 
